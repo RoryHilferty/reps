2fcafebd91df32ed9683199a055dbbb60cf37193
Rory Hilferty <rory.hilferty@outlook.com>
2018-12-12 18:58:55 +0000

    Simplify the javascript

--- js/main.js
+++ js/main.js
@@ -1,11 +1,7 @@
 var menuWrapper = document.querySelector('.menuWrapper');
 
-menuWrapper.addEventListener('click', function () {
+menuWrapper.addEventListener('click', function() {
   var menuLine = document.querySelector('.menuLine');
 
-  if (menuLine.classList.contains('clicked')) {
-    menuLine.classList.remove('clicked');
-  } else {
-    menuLine.classList.add('clicked');
-  }
+  menuLine.classList.toggle('toggled');
 });
