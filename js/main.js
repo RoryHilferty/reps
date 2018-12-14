24a0509301a490ca4563b7d1e1c314cd0f160cc1
Rory Hilferty <rory.hilferty@outlook.com>
2018-12-14 13:41:32 +0000

    Add javascript toggle 'toggled' class on nav menu

--- js/main.js
+++ js/main.js
@@ -1,19 +1,15 @@
 $(document).ready(function() {
-  setTimeout(function(){
-    window.scrollTo(0, 1); // This hides the address bar:
-}, 0);
-
   var menuWrapper = document.querySelector('.menuWrapper');
   
   menuWrapper.addEventListener('click', function() {
-    var menuLine = document.querySelector('.menuLine');
-    
-    menuLine.classList.toggle('toggled');
+    document.querySelector('.menuLine').classList.toggle('toggled');
+    document.querySelector('.navOpened').classList.toggle('toggled');
+    document.querySelector('.navLeft').classList.toggle('toggled');
   });
 
   var arrow = document.querySelector('.arrow');
 
   arrow.addEventListener('click', function() {
-    $('.arrow').animatescroll({scrollSpeed: 750, easing: 'easeInOutQuad'});
+    $('#app').animatescroll({scrollSpeed: 750, easing: 'easeInOutQuad'});
   });
 });
