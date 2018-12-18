b9eef1eb25983aaae8436ab9c18aab866e14dd88
Rory Hilferty <rory.hilferty@outlook.com>
2018-12-18 23:59:00 +0000

    Add javascript for input fields

--- js/main.js
+++ js/main.js
@@ -5,11 +5,33 @@ $(document).ready(function() {
     document.querySelector('.menuLine').classList.toggle('toggled');
     document.querySelector('.navOpened').classList.toggle('toggled');
     document.querySelector('.navLeft').classList.toggle('toggled');
+
+    var inputLineList =  document.querySelectorAll('.inputLine');
+
+    for (inputLine of inputLineList) {
+      inputLine.classList.toggle('toggled');
+    }
   });
 
   var arrow = document.querySelector('.arrow');
 
   arrow.addEventListener('click', function() {
-    $('#app').animatescroll({scrollSpeed: 750, easing: 'easeInOutQuad'});
+    $('#app').animatescroll({scrollSpeed: 1500, easing: 'easeInOutQuint'});
   });
+
+  var inputList =  document.querySelectorAll('input');
+
+  for (input of inputList) {
+    input.addEventListener('focus', function(ev) {
+      ev.target.parentElement.classList.add('effect');
+    });
+
+    input.addEventListener('blur', function(ev) {
+      if (ev.target.value == '') {
+        ev.target.parentElement.classList.remove('effect');
+      }
+    });
+  }
+
+  var rallax = new Rellax('.rellax');
 });
