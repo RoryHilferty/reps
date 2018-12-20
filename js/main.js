d7183fe70b5248ed080c95b951a53cc17b35c61d
Rory Hilferty <rory.hilferty@outlook.com>
2018-12-20 00:28:23 +0000

    Add wathan formula, improve ui/ux

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
+  // Define Wathan's formula for calculating 1RM
+  function wathan(w, r) {
+    // Get user inputed values
+    w = parseFloat(document.querySelector('.weight').value);
+    r = parseFloat(document.querySelector('.reps').value);
+
+    var result = (100 * w) / (48.8 + 53.8 * Math.exp(-0.075 * r));
+    // Correct result to 1 decimal place
+    return result.toFixed(1);
+  }
+
+  // Required for Rellax plug-in
   var rallax = new Rellax('.rellax');
+
   var menuWrapper = document.querySelector('.menuWrapper');
   
   menuWrapper.addEventListener('click', function() {
@@ -33,4 +46,21 @@ $(document).ready(function() {
       }
     });
   }
+
+  document.querySelector('.btn').addEventListener('click', function() {
+    setTimeout(function() {
+      $('.weight').animatescroll({scrollSpeed: 1000, easing: 'easeInOutQuint', padding: 110});
+    }, 100)
+
+    result = document.getElementById('result');
+    result.classList.add('result');
+
+    if (document.querySelector('.weight').value == '' || document.querySelector('.reps').value == '') {
+      result.innerHTML = 0;
+    } else {
+      result.innerHTML = wathan();
+    }
+
+
+  });
 });
