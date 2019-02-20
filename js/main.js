d52358c91622a4efbb417a239d94eb123c91dc5c
Rory Hilferty <rory.hilferty@outlook.com>
2019-02-20 22:17:02 +0000

    Add final itterations before remodel

--- js/main.js
+++ js/main.js
@@ -11,13 +11,12 @@ $(document).ready(function() {
       return weight;
     } else {
       var result = (100 * weight) / (48.8 + 53.8 * Math.exp(-0.075 * reps));
-      // Correct result to 1 decimal place
-      return result.toFixed(1);
+      return result;
     }
   }
 
   // Required for Rellax plug-in
-  var rallax = new Rellax('.rellax');
+  // var rallax = new Rellax('.rellax');
 
   // Get wrapper for burger menu
   var menuWrapper = document.querySelector('.menuWrapper');
@@ -29,9 +28,9 @@ $(document).ready(function() {
   });
 
   // Smooth scroll to app when arrow is clicked
-  document.querySelector('.arrow').addEventListener('click', function() {
-    $('#app').animatescroll({scrollSpeed: 1500, easing: 'easeInOutQuint'});
-  });
+  // document.querySelector('.arrow').addEventListener('click', function() {
+  //   $('#app').animatescroll({scrollSpeed: 1500, easing: 'easeInOutQuint'});
+  // });
 
   var inputArray =  document.querySelectorAll('input');
 
@@ -39,11 +38,11 @@ $(document).ready(function() {
     item.addEventListener('focus', function(ev) {
       ev.target.parentElement.classList.add('effect');
 
-      if (screen.width <= 800) {
-        setTimeout(function() {
-          $('.weight').animatescroll({scrollSpeed: 1500, easing: 'easeInOutQuint', padding: 110});
-        }, 200)
-      }
+      // if (screen.width <= 800) {
+      //   setTimeout(function() {
+      //     $('.weight').animatescroll({scrollSpeed: 1500, easing: 'easeInOutQuint', padding: 110});
+      //   }, 200)
+      // }
     });
 
     item.addEventListener('blur', function(ev) {
@@ -57,14 +56,48 @@ $(document).ready(function() {
   document.querySelector('.btn').addEventListener('click', function() {
     if (screen.width <= 800) {
       setTimeout(function() {
-        $('.weight').animatescroll({scrollSpeed: 1500, easing: 'easeInOutQuint', padding: 110});
+        $('#result').animatescroll({scrollSpeed: 1500, easing: 'easeInOutQuint', padding: 100});
       }, 200)
     }
 
     result = document.getElementById('result');
     result.classList.add('result');
-    result.innerHTML = wathan();
 
+    // Correct result to 0 decimal places
+    result.innerHTML = wathan().toFixed(0);
+
+    // Create object with all multipliers for the repmaxs
+    var multipliers = {
+      1: 1,
+      2: 0.95106,
+      3: 0.91760,
+      4: 0.88656,
+      5: 0.85776,
+      6: 0.83104,
+      7: 0.80626,
+      8: 0.78326,
+      9: 0.76193,
+      10: 0.74213
+    }
+
+    if (document.querySelector('.tableWrapper').classList.contains('hidden')) {
+      document.querySelector('.tableWrapper').classList.remove('hidden');
+    }
     
+    document.querySelector('table').innerHTML = `
+    <tr>
+      <th>reps</th>
+      <th>weight</th>
+    </tr>
+    `;
+
+    for (i = 1; i < 11; i++) {
+      document.querySelector('table').innerHTML += `
+        <tr>
+          <td>${i}</td>
+          <td>${(wathan() * multipliers[i]).toFixed(1)}</td>
+        </tr>
+      `;
+    }
   });
 });
