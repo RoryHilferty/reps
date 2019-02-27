c73494317b1fada91d3db9a05e4c3b010f66e27e
Rory Hilferty <rory.hilferty@outlook.com>
2019-02-27 16:22:11 +0000

    Define Wathans formula, toggle effect classes for inputs on focus/blur

--- js/main.js
+++ js/main.js
@@ -1,7 +1,37 @@
 window.onload = function() {
-  var menuBtn = document.querySelector('.menuBtn');
+  // Define Wathans formula for calculating 1RM
+  function wathan(weight, reps) {
+    weight = parseFloat(document.querySelector('.weight').value);
+    reps = parseFloat(document.querySelector('.reps').value);
+
+    // Detect if input values are invalid
+    if (reps == 0 || document.querySelector('.weight').value == '' || document.querySelector('.reps').value == '') {
+      return 0;
+    } else if (reps == 1) {
+      return weight;
+    } else {
+      var result = (100 * weight) / (48.8 + 53.8 * Math.exp(-0.075 * reps));
+      return result;
+    }
+  }
+
+  const menuBtn = document.querySelector('.menuBtn');
 
   menuBtn.addEventListener('click', function() {
     menuBtn.classList.toggle('open');
   });
+
+  const inputList = document.querySelectorAll('input');
+
+  for (item of inputList) {
+    item.addEventListener('focus', function(event) {
+      event.target.parentElement.classList.add('effect');
+    });
+
+    item.addEventListener('blur', function(event) {
+      if (event.target.value == '') {
+        event.target.parentElement.classList.remove('effect');
+      }
+    });
+  }
 }
