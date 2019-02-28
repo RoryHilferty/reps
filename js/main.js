d4a5d16e1cb674d7e09ec7277b50faa405889c69
Rory Hilferty <rory.hilferty@outlook.com>
2019-02-28 01:58:00 +0000

    Generate the rep maxes table on calculation

--- js/main.js
+++ js/main.js
@@ -39,6 +39,7 @@ window.onload = function() {
   
   calculate.addEventListener('click', function() {
     const resultArea = document.querySelector('.resultArea');
+    const repMaxes = document.querySelector('.repMaxes');
 
     if (resultArea.classList.contains('notVisible')) {
       resultArea.classList.remove('notVisible');
@@ -47,8 +48,47 @@ window.onload = function() {
     // Detect if returned value is ridiculously large
     if (wathan() > 10000) {
       resultArea.innerHTML = 'O O F';
+
+      if (repMaxes.classList.contains('notVisible') == false) {
+        repMaxes.classList.add('notVisible');
+      }
     } else {
       resultArea.innerHTML = wathan().toFixed(0);
+    }
+    
+    const multipliers = {
+      1: 1,
+      2: 0.95106,
+      3: 0.91760,
+      4: 0.88656,
+      5: 0.85776,
+      6: 0.83104,
+      7: 0.80626,
+      8: 0.78326,
+      9: 0.76193,
+     10: 0.74213
+    }
+
+    if (wathan() <= 10000) {
+      if (repMaxes.classList.contains('notVisible')) {
+        repMaxes.classList.remove('notVisible');
+      }
+
+      repMaxes.innerHTML = `
+        <tr>
+          <th>Reps</th>
+          <th>Weight</th>
+        </tr>
+      `;
+
+      for (i = 1; i <= Object.keys(multipliers).length; i++) {
+        repMaxes.innerHTML += `
+          <tr>
+            <td>${i}</td>
+            <td>${(wathan() * multipliers[i]).toFixed(1)}</td>
+          </tr>
+        `;
+      }
     }
   });
 }
